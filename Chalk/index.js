6e35c8b813d7d20585a48b5cc32d4f718877fce1
gunn989 <wgdereu@gmail.com>
2019-11-11 15:06:48 -0600

    Add files via upload

---
+++ Chalk/index.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('There is a well-known pirate that goes by the name Dread Pirate Roberts.'));
+console.log(chalk.red('He is feared throughout the land. He is known to never leaves captives alive.'));
+console.log(chalk.green('However, Dread Pirate Roberts has a little secret.'));
+console.log(chalk.yellow('You see, Dread Pirate Roberts is actually not one person, but a series of indiciduals who periodically pass the name and reputation to a chosen successor.'));
+console.log(chalk.cyan('Everyone except the successor is then released at a convenient port, a new crew hiredm and the former Roberts stats aboard as first mate.'));
+console.log(chalk.magenta('The constant reference to Captain Roberts establishes the new Roberts Persona.'));
+console.log(chalk.whiteBright('After the crew is convinced, the former Roberts leaves the ship and retires on his earnings.'));
+
+
+
+
